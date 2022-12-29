4162860f79062c34bffc06bf9368ac794527a5f6
Riya Shah <riyaaa.2502shahhh@gmail.com>
2022-12-29 15:32:31 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Thats the First project of MLops using DVC',
+    author='Riya20-shah',
+    license='MIT',
+)
