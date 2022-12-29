fbdfb84e2489ee42dad7b236e5f06b075deeabfb
Riya Shah <riyaaa.2502shahhh@gmail.com>
2022-12-29 17:12:41 +0530

    config added

---
+++ src/pipeline_01_data_preparation.py
@@ -0,0 +1,14 @@
+import os
+import argparse
+import yaml
+import logging
+
+
+
+if __name__=="__main__":
+    arg = argparse.ArgumentParser()
+    arg.add_argument("--config",default="Default")
+    arg.add_argument("--datasource" , default=None)
+
+    parsed_arg = arg.parse_args()
+    print(parsed_arg.config , parsed_arg.datasource)
