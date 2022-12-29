9fda0c900ec25261831ce538fcf2702d9efeaa60
Riya Shah <riyaaa.2502shahhh@gmail.com>
2022-12-29 18:03:56 +0530

    read config

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -4,11 +4,22 @@ import yaml
 import logging
 
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
 
-if __name__=="__main__":
-    arg = argparse.ArgumentParser()
-    arg.add_argument("--config",default="Default")
-    arg.add_argument("--datasource" , default=None)
 
-    parsed_arg = arg.parse_args()
-    print(parsed_arg.config , parsed_arg.datasource)
+def main(config_path, datasource):
+    config = read_params(config_path)
+    print(config)
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    default_config_path = "/home/scaledge-riya/Desktop/MLops_demo_projects/First_Mlops/config/params.yaml"
+    args.add_argument("--config", default=default_config_path)
+    args.add_argument("--datasource", default=None)
+
+    parsed_args = args.parse_args()
+    main(config_path=parsed_args.config, datasource=parsed_args.datasource)
